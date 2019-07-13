237b84771ca8a736d83933dd4a4e7711c83e4dee
cwh <1111*qqqq>
2019-07-13 15:12:04 +0800

    service层

---
+++ myspringboot-service/src/main/java/com/cwh/myspringbootservice/MyspringbootServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cwh.myspringbootservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyspringbootServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyspringbootServiceApplication.class, args);
+    }
+
+}
